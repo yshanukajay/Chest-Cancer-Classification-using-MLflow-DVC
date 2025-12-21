0ea9d4df9f6d1dc63f3fcd202844912a38ad71a4
JADY Shanuka <2021ICTS28@vauitc.com>
2025-12-22 04:59:39 +0530

    prepare base model stage completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chestCNN import logger
 from chestCNN.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from chestCNN.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -11,3 +12,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
