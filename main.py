29edd773dbbfa763837a2348edf5615ab92b6bc1
JADY Shanuka <2021ICTS28@vauitc.com>
2025-12-30 11:44:19 +0530

    model evaluated using mlflow

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
 from chestCNN import logger
 from chestCNN.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from chestCNN.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from chestCNN.pipeline.stage03_model_trainer import ModelTrainingPipeline
+from chestCNN.pipeline.stage04_model_evaluation import EvaluationPipeline
+
+# Set MLflow tracking environment variables
+os.environ['MLFLOW_TRACKING_URI'] = "https://dagshub.com/YohanJay23/Chest-Cancer-Classification-using-MLflow-DVC.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME'] = "YohanJay23"
+os.environ['MLFLOW_TRACKING_PASSWORD'] = "12aa8bae310f0795c7c23dcd43e1bb74fec38ea1"
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -14,7 +21,7 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-
+    
 STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
@@ -37,3 +44,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+    
+    
+STAGE_NAME = "Evaluation stage"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+            
